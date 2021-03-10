9af9020d2905ac2d1c12f92a52dde5c1668e5a65
Dodo30 <d.pochciol@gmail.com>
2021-03-10 16:10:24 +0100

    commit dp

--- src/components/Diashow.js
+++ src/components/Diashow.js
@@ -4,9 +4,10 @@ import pic1 from '../img/pic1.jpg';
 import pic2 from '../img/pic2.jpg';
 import pic3 from '../img/pic3.jpg';
 import pic4 from '../img/pic4.jpg';
+import pic5 from '../img/pic5.jpg';
 
 const Diashow = () => {
-  const [pic, setPic] = useState([pic1, pic2, pic3, pic4]);
+  const [pic, setPic] = useState([pic1, pic2, pic3, pic4, pic5]);
   const [count, setCount] = useState(0);
   const [current, setCurrent] = useState(pic1);
 
